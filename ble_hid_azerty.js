ed34f16edbf3b5b5ff75ad93c03802109498951d
Yobio <81526725+yobio@users.noreply.github.com>
2021-05-24 10:52:20 +0200

    Update ble_hid_azerty.js

--- ble_hid_azerty.js
+++ ble_hid_azerty.js
@@ -222,6 +222,7 @@ sendHID = exports.sendHID = function(i,strArray) {
 	NRF.sendHIDReport([0,0,KEY[strArray[charNb]],0,0,0,0,0], function() {
 		if (charNb < strArray.length - 1) {
 			charNb += 1;
+			if (charNb > strArray.length) break;
 			sendHID(charNb,strArray);
 		}
 	});
