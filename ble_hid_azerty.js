f3fb77d2087a9954853f09656996b7af6eb68b56
Yobio <81526725+yobio@users.noreply.github.com>
2022-02-02 17:39:00 +0100

    Update ble_hid_azerty.js

--- ble_hid_azerty.js
+++ ble_hid_azerty.js
@@ -238,9 +238,11 @@ let sendHID = exports.sendHID = function(charNb, string, callback) {
     NRF.sendHIDReport([modifier,0,KEY[string[charNb].toUpperCase()],0,0,0,0,0], function() {
         if (charNb < string.length - 1) {
             charNb += 1;
-            sendHID(charNb,string, function() {
-              if (callback) callback();
-            });
+            setTimeOut(function() {
+              sendHID(charNb,string, function() {
+                if (callback) callback();
+              });
+            }, 0);
 		} else {
 			NRF.sendHIDReport([0,0,0,0,0,0,0,0], function() {
               if (callback) callback();
